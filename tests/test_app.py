ad87fdac8df7416265f9e23b9c6305b683186263
TheBleu <lotfiwork@outlook.fr>
2025-12-20 14:01:25 +0100

    "small comment change"

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,8 +4,4 @@ from demo import app
 def test_index_route():
     client = app.test_client()
     response = client.get("/index")
-    assert response.status_code == 200
-
-
-    # ya kho lazm tpushiha l github
-    
+    assert response.status_code == 200    
