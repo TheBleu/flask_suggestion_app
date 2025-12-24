99d48a2491f72fcc20e671d63e3719dcef77a766
TheBleu <lotfiwork@outlook.fr>
2025-12-25 00:53:08 +0100

    fixing the issue with path import

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,12 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from demo import app
 
 
 def test_index_route():
     client = app.test_client()
     response = client.get("/index")
-    assert response.status_code == 200    
+    assert response.status_code == 200
